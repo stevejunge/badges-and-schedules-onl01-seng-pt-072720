0e6f0d59d1d92cae84a35a24b0483d15d6424504
Stephen Junge <steve.junge@gmail.com>
2020-08-05 02:21:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,8 +16,13 @@ def assign_rooms(speakers)
   return greet
   end
 
-def printer(inputOne)
-  batch_badge_creator(inputOne)
-  assign_rooms(inputOne)
-
+def printer(attendees)
+  resultOne = batch_badge_creator(attendees)
+  resultOne.each do |x|
+    puts x 
+  end
+  result = assign_rooms(attendees)
+  result.each do |x|
+    puts x 
+  end
 end
