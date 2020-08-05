8848ef25596480b60a8500eae343a64fb07e87ae
Stephen Junge <steve.junge@gmail.com>
2020-08-05 01:45:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+  name.each do |name|
+    puts "Hello, my name is #{name}."
+  end
+  return badge_maker
+end
+
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+    badges.push("Hello, my name is #{name}.")
+  
+  end
+end
